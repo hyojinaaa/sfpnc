5f43c5ccaa523dbc1365baa3206cfeb610ce6b00
hyojinaaa <hyojinjin@hotmail.com>
2016-11-27 16:53:58 +1300

    Finished architecture

---
+++ category-seminars.php
@@ -0,0 +1,25 @@
+<?php
+
+get_header();
+?>
+
+
+<?php
+if( have_posts() ) :
+	while (have_posts()) : the_post(); ?>
+
+	<h2><?php the_title(); ?></h2>
+	<p><?php the_content(); ?></p>
+
+	<?php endwhile;
+else:
+	echo '<p>No content found</p>';
+
+endif; ?>
+
+<h2>This is Seminars page</h2>
+
+<?php
+get_footer();
+
+?>
--- page.php
+++ page.php
@@ -1,24 +1,9 @@
 <?php
 
-get_header();
+get_header(); ?>
 
+<h2>This is About us Page</h2>
 
-if( have_posts() ) :
-	while (have_posts()) : the_post(); ?>
-
-	<h2><?php the_title(); ?></h2>
-	<p><?php the_content(); ?></p>
-
-	<?php endwhile;
-else:
-	echo '<p>No content found</p>';
-
-endif;
-
-
-
-get_footer();
-
-
+<?get_footer();
 
 ?>
