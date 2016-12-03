f70335f9a8aabb44594d3cbf35eb9a50d36a79dc
hyojinaaa <hyojinjin@hotmail.com>
2016-12-04 02:01:32 +1300

    Finished homepage

--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
 <footer>
   <div class="site-footer">
 
-  <div class="social-network">
-    <a href=""><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
-    <a href=""><i class="fa fa-envelope" aria-hidden="true"></i></a>
-  </div>
-
   <div class="uni-links links">
     <a href=""><p>Victoria University  |  NZ</p></a>
     <a href=""><p>McMaster University  |  Canada</p></a>
+  </div>
+
+  <div class="social-network">
+    <a href=""><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+    <a href=""><i class="fa fa-envelope" aria-hidden="true"></i></a>
   </div>
 
   <hr>
@@ -19,7 +19,7 @@
 
   <div class="developer-links links">
     <p>Logo designed by <strong>Frances Denton</strong></p>
-    <p>Website design and developed by <strong><a href="http://jinawebdev.com" class="hyojin">Hyojin Jung</a></strong></p>
+    <p>Website design and developed by <strong><a href="http://jinawebdev.com">Hyojin Jung</a></strong></p>
   </div>
 
 </footer>
