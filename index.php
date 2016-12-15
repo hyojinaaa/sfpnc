c100bf46b8192a02e66e45f2dc4864757efb3b5e
hyojinaaa <hyojinjin@hotmail.com>
2016-12-15 23:33:30 +1300

    Looping ID

--- index.php
+++ index.php
@@ -45,13 +45,24 @@ get_header(); ?>
 ?>
 <?php if ( $upcoming_query->have_posts() ) : ?>
   <!-- the loop -->
+	<section>
+	<ul class="carousel">
+		<?php $number = 1; ?>
   <?php while ( $upcoming_query->have_posts() ) : $upcoming_query->the_post(); ?>
-<h2><?php the_title(); ?>
+
   	<!--Your Content-->
+
+	    <li class="items main-pos" id="<?php echo $number; ?>"><h2><?php the_title(); ?></h2></li>
+			<?php $number++; ?>
 
   <?php endwhile; ?>
   <!-- end of the loop -->
-
+</ul>
+<span>
+<input type="button" value="Prev" id="prev">
+<input type="button" value="Next" id="next">
+</span>
+</section>
   <?php wp_reset_postdata(); ?>
 
 <?php else:  ?>
@@ -65,3 +76,112 @@ get_header(); ?>
 <?php get_footer();
 
 ?>
+
+<script type="text/javascript">
+//slideshow style interval
+// var autoSwap = setInterval( swap,3500);
+//
+// //pause slideshow and reinstantiate on mouseout
+// $('ul, span').hover(
+// function () {
+// 	clearInterval(autoSwap);
+// },
+// function () {
+//  autoSwap = setInterval( swap,3500);
+// });
+//
+// //global variables
+// var items = [];
+// var startItem = 1;
+// var position = 0;
+// var itemCount = $('.carousel li.items').length;
+// var leftpos = itemCount;
+// var resetCount = itemCount;
+//
+// //unused: gather text inside items class
+// $('li.items').each(function(index) {
+// 	items[index] = $(this).text();
+// });
+//
+// //swap images function
+// function swap(action) {
+// var direction = action;
+//
+// //moving carousel backwards
+// if(direction == 'counter-clockwise') {
+// 	var leftitem = $('.left-pos').attr('id') - 1;
+// 	if(leftitem == 0) {
+// 		leftitem = itemCount;
+// 	}
+//
+// 	$('.right-pos').removeClass('right-pos').addClass('back-pos');
+// 	$('.main-pos').removeClass('main-pos').addClass('right-pos');
+// 	$('.left-pos').removeClass('left-pos').addClass('main-pos');
+// 	$('#'+leftitem+'').removeClass('back-pos').addClass('left-pos');
+//
+// 	startItem--;
+// 	if(startItem < 1) {
+// 		startItem = itemCount;
+// 	}
+// }
+//
+// //moving carousel forward
+// if(direction == 'clockwise' || direction == '' || direction == null ) {
+// 	function pos(positionvalue) {
+// 		if(positionvalue != 'leftposition') {
+// 			//increment image list id
+// 			position++;
+//
+// 			//if final result is greater than image count, reset position.
+// 			if((startItem+position) > resetCount) {
+// 				position = 1-startItem;
+// 			}
+// 		}
+//
+// 		//setting the left positioned item
+// 		if(positionvalue == 'leftposition') {
+// 			//left positioned image should always be one left than main positioned image.
+// 			position = startItem - 1;
+//
+// 			//reset last image in list to left position if first image is in main position
+// 			if(position < 1) {
+// 				position = itemCount;
+// 			}
+// 		}
+//
+// 		return position;
+// 	}
+//
+//  $('#'+ startItem +'').removeClass('main-pos').addClass('left-pos');
+//  $('#'+ (startItem+pos()) +'').removeClass('right-pos').addClass('main-pos');
+//  $('#'+ (startItem+pos()) +'').removeClass('back-pos').addClass('right-pos');
+//  $('#'+ pos('leftposition') +'').removeClass('left-pos').addClass('back-pos');
+//
+// 	startItem++;
+// 	position=0;
+// 	if(startItem > itemCount) {
+// 		startItem = 1;
+// 	}
+// }
+// }
+//
+// //next button click function
+// $('#next').click(function() {
+// swap('clockwise');
+// });
+//
+// //prev button click function
+// $('#prev').click(function() {
+// swap('counter-clockwise');
+// });
+//
+// //if any visible items are clicked
+// $('li').click(function() {
+// if($(this).attr('class') == 'items left-pos') {
+// 	 swap('counter-clockwise');
+// }
+// else {
+// 	swap('clockwise');
+// }
+// });
+</script>
