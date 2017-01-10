ad915e30bcd2f6640127845fce2a4dc6e6bee2f6
hyojinaaa <hyojinjin@hotmail.com>
2017-01-10 19:56:57 +1300

    Disable mailchimp plugin

--- slider.js
+++ slider.js
@@ -1,71 +1,31 @@
-//$(window).bind('load', function() {
-    // $.ajax({
-    //     url: 'archive-seminars.php',
-    //     success: function(data){
-    //         $('#result-container').html(data);
-    //         //$('#result-container article').html(data);
-    //         slideSwitch(); //Initialize slider after elements are loaded into the DOM
-    //     }
-    // });
-
-    // $.ajax({
-    //     url: 'archive-seminars.php',
-    //     success: function(data){
-    //         $('#result-container article').html(data);
-    //         slideSwitch(); //Initialize slider after elements are loaded into the DOM
-    //         console.log(data);
-    //     }
-    // });
-
-
-    // // Get all the links of the articles
-    // var allArticles = document.querySelectorAll('#result-container article');
-    // var container = document.querySelector('#result-container');
-    //
-    // // Grab an image attribute src from console
-    // var articleArray = [];
-    //
-    // // Push image src into an array 'imageSource'
-    // for ( var i=0; i<allArticles.length; i++ ){
-    //
-    // 	articleArray.push( allArticles[i].src );
-    //
-    // }
-
-    //console.log(container);
-    //console.log(articleArray);
-//});
-
 $(document).ready(function(){
 
   $.ajax({
      url: 'https://api.github.com/repos/vmg/redcarpet/issues?state=closed',
 
 success: function(data){
-console.log(data);
 
-          // Loop over the JSON that comes back from the GitHub links
+    console.log(data);
+    $.each(data, function(index, value){
 
-          // Use the below code to create new article and HTML elements
+    // Create the h1 element for the title
+    $h1 = $('<h1>');
+    // Add the title text to the h1 element
+    $($h1).html(data[index].title);
 
-          // Fill those elements with any data you want from the GitHub data
+    // For the text
+    $text = $('<p>');
+    $($text).html(data[index].body);
 
-          // Create new HTML element
-          $article = $('<article>');
+    // The article itself
+    $article = $('<article>');
+    $($article).append($h1);
+    $($article).append($text);
 
-          $h1 = $('<h1>');
+    // Put it on the screen
+    $('#result-container').append($article);
+    });
 
-          // Put data into h1 element (change this to say a name from the GitHub Data)
-          $($h1).html(data.title);
-
-          // Put the h1 into the article
-          $($article).append($h1);
-
-
-          // Put the article into the page
-         $('#result-container').append($article);
-
-         //slideSwitch();
      },
 error: function(){
 alert('if this alert shows then AJAX is not connecting to your PHP file correctly');
