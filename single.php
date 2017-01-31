667d32e1a390ecc69cfa9fe828d3908602c166d1
hyojinaaa <hyojinjin@hotmail.com>
2017-01-31 21:50:58 +1300

    Add respondants

--- single.php
+++ single.php
@@ -16,14 +16,94 @@ get_header(); ?>
      <div class="single-image"><?php the_post_thumbnail('poster'); ?></div>
     <?php } ?>
      <div class="info <?php if ( has_post_thumbnail() == null ) { ?> no-image-info<?php } ?>">
-       <p class="single-info"><strong>Speakers : </strong><a href="<?php echo the_field('main_speaker_personal_web_link'); ?>"><?php the_field('main_speaker_name'); ?></a>(<?php if(get_the_term_list( $post->ID, 'main_speaker_second_affiliation_')) {echo get_the_term_list( $post->ID, 'main_speaker_second_affiliation_'); }?><?php if(get_field('main_speaker_department_/_school')){ ?>, <a href="<?php echo the_field('main_speker_d/s_web_link'); ?>"><?php the_field('main_speaker_department_/_school'); ?></a><?php } ?><?php if(get_the_term_list( $post->ID, 'main_speaker_uni')){ ?><?php } ?><?php if(get_the_term_list( $post->ID, 'main_speaker_uni')): ?>, <?php echo get_the_term_list( $post->ID, 'main_speaker_uni'); ?><?php endif; ?>)</p>
+       <p class="single-info"><strong>Speakers : </strong><a href="<?php echo the_field('main_speaker_personal_web_link'); ?>"><?php the_field('main_speaker_name'); ?></a> (
 
-       <?php if( get_field('respondent_1_name') ): ?>
-        <p class="single-info"><strong>Respondent: </strong><a href="<?php echo the_field('respondent_1_personal_web_link'); ?>"><?php the_field('respondent_1_name'); ?></a>
-          <?php if(get_field('respondent_1_second_affiliation/title') || get_field('respondent_1_department/school') || get_field('respondent_1_university')): ?>(<?php the_field('respondent_1_second_affiliation/title'); ?><?php endif; ?><?php if(get_field('respondent_1_department/school') ): ?>, <a href="<?php echo the_field('respondent_1_d/s_web_link'); ?>"><?php the_field('respondent_1_department/school'); ?></a><?php endif; ?><?php if(get_field('respondent_1_university')): ?>,  <?php the_field('respondent_1_university'); ?> <?php endif; ?>)<?php endif; ?>
+         <?php if(get_the_term_list( $post->ID, 'main_speaker_second_affiliation_')) {
+           echo get_the_term_list( $post->ID, 'main_speaker_second_affiliation_');
+         }?>
 
-            <?php if( get_field('respondent_2_name') ): ?>,
-         <a href="<?php echo the_field('respondent_2_personal_web_link'); ?>"><?php the_field('respondent_2_name'); ?></a>(<?php the_field('respondent_2_second_affiliation/title'); ?>, <a href="<?php echo the_field('respondent_2_d/s_web_link'); ?>"><?php the_field('respondent_2_department/school'); ?></a>,  <?php the_field('respondent_2_university'); ?>)<?php endif; ?> </p>
+           <?php if(get_field('main_speaker_department_/_school')){
+             ?>, <a href="<?php echo the_field('main_speker_d/s_web_link'); ?>"><?php the_field('main_speaker_department_/_school'); ?></a>
+           <?php } ?>
+
+        <?php if(get_the_term_list( $post->ID, 'main_speaker_uni')){ ?>
+          , <?php echo get_the_term_list( $post->ID, 'main_speaker_uni'); ?>
+        <?php } ?>)
+      </p>
+
+<!-- Respondent 1 start -->
+<?php if( get_field('respondent_1_name') ): ?>
+  <p class="single-info"><strong>Respondent: </strong>
+
+    <a href="<?php echo the_field('respondent_1_personal_web_link'); ?>"><?php the_field('respondent_1_name'); ?></a>
+
+      <?php if(get_field('respondent_1_second_affiliation/title') || get_field('respondent_1_department/school') || get_field('respondent_1_university')): ?>
+
+        (<?php the_field('respondent_1_second_affiliation/title'); ?>
+
+      <?php endif; ?>
+
+      <?php if( get_field('respondent_1_department/school') ||  get_field('respondent_1_university')): ?>
+        <?php if( get_field('respondent_1_second_affiliation/title') ): ?>
+          , <?php endif; ?><a href="<?php echo the_field('respondent_1_d/s_web_link'); ?>"><?php the_field('respondent_1_department/school'); ?></a>
+      <?php endif; ?>
+
+      <?php if(get_field('respondent_1_university')): ?>
+        <?php if( get_field('respondent_1_second_affiliation/title') || get_field('respondent_1_department/school')): ?>
+        , <?php endif; ?><?php the_field('respondent_1_university'); ?>
+      <?php endif; ?>
+  )</p>
+<?php endif; ?>
+
+<!-- Respondent 2 start -->
+<?php if( get_field('respondent_2_name') ): ?>
+  <p class="single-info"><strong>Respondent: </strong>
+
+    <a href="<?php echo the_field('respondent_2_personal_web_link'); ?>"><?php the_field('respondent_2_name'); ?></a>
+
+      <?php if(get_field('respondent_2_second_affiliation/title') || get_field('respondent_2_department/school') || get_field('respondent_2_university')): ?>
+
+        (<?php the_field('respondent_2_second_affiliation/title'); ?>
+
+      <?php endif; ?>
+
+      <?php if( get_field('respondent_2_department/school') ||  get_field('respondent_2_university')): ?>
+        <?php if( get_field('respondent_2_second_affiliation/title') ): ?>
+          , <?php endif; ?><a href="<?php echo the_field('respondent_2_d/s_web_link'); ?>"><?php the_field('respondent_2_department/school'); ?></a>
+      <?php endif; ?>
+
+      <?php if(get_field('respondent_2_university')): ?>
+        <?php if( get_field('respondent_2_second_affiliation/title') || get_field('respondent_2_department/school')): ?>
+        , <?php endif; ?><?php the_field('respondent_2_university'); ?>
+      <?php endif; ?>
+  )</p>
+<?php endif; ?>
+
+<!-- Respondent 1 start -->
+<?php if( get_field('respondent_3_name') ): ?>
+  <p class="single-info"><strong>Respondent: </strong>
+
+    <a href="<?php echo the_field('respondent_3_personal_web_link'); ?>"><?php the_field('respondent_3_name'); ?></a>
+
+      <?php if(get_field('respondent_3_second_affiliation/title') || get_field('respondent_3_department/school') || get_field('respondent_3_university')): ?>
+
+        (<?php the_field('respondent_3_second_affiliation/title'); ?>
+
+      <?php endif; ?>
+
+      <?php if( get_field('respondent_3_department/school') ||  get_field('respondent_3_university')): ?>
+        <?php if( get_field('respondent_3_second_affiliation/title') ): ?>
+          , <?php endif; ?><a href="<?php echo the_field('respondent_3_d/s_web_link'); ?>"><?php the_field('respondent_3_department/school'); ?></a>
+      <?php endif; ?>
+
+      <?php if(get_field('respondent_3_university')): ?>
+        <?php if( get_field('respondent_3_second_affiliation/title') || get_field('respondent_3_department/school')): ?>
+        , <?php endif; ?><?php the_field('respondent_3_university'); ?>
+      <?php endif; ?>
+  )</p>
+<?php endif; ?>
+
+
 
 
     <?php if(get_field("date_and_time") ||  get_field("date")) { ?>
@@ -38,32 +118,6 @@ get_header(); ?>
        <p class="single-info"><strong>Venue : </strong><?php the_field('venue'); ?></p>
     <?php } ?>
 
-    <?php if(get_the_term_list( $post->ID, 'series')) { ?>
-       <p class="single-info"><strong>Series : </strong><?php echo get_the_term_list( $post->ID, 'series'); ?></p>
-    <?php } ?>
-
-    <?php if(get_the_category()){ ?>
-       <p class="single-info"><strong>Disciplines : </strong>	<?php
-
-			$categories = get_the_category();
-			$separator = ", ";
-			$output = '';
-
-			if ($categories) {
-
-				foreach ($categories as $category) {
-
-					$output .= '<a href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>'  . $separator;
-
-				}
-
-				echo trim($output, $separator);
-
-			}
-
-			?>
-			</p>
-      <?php } ?>
      </div>
 
    </div>
